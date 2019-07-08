b4257b19848688191635d41a8571cdc5158dc5a8
jiahuanluo <luojiahuan001@gmail.com>
2019-07-08 12:11:35 +0800

    update script

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DATASET=$1
 NUM_CLIENT=$2
+MODEL=$3
 echo ${NUM_CLIENT}
 for i in $(seq 1 ${NUM_CLIENT}); do
-    nohup python3 federated_learning/fl_client.py $(($i % 8)) data/task_configs/${DATASET}/faster_rcnn_task$i.json True &
+    nohup python3 fl_client.py $(($i % 8)) data/task_configs/${DATASET}/${MODEL}_task_$i.json True &
 done
--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DATASET=$1
-python3 federated_learning/fl_server.py data/task_configs/${DATASET}/faster_rcnn_task.json
+MODEL=$2
+python3 federated_learning/fl_server.py data/task_configs/${DATASET}/${MODEL}_task.json
