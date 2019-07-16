15da514ab026e992c8ffbe1b0cf2d89966f3d047
jiahuanluo <luojiahuan001@gmail.com>
2019-07-16 14:25:21 +0800

    update script

--- run_server.sh
+++ run_server.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+
+set -x
+set -e
+
 DATASET=$1
 MODEL=$2
 PORT=$3
-if [ ! -n "$DATASET" ];then
+
+if ( ! -n "$DATASET" );then
 	echo "Please input dataset"
 	exit
 fi
 
-if [ ! -n "$MODEL" ];then
+if ( ! -n "$MODEL" );then
         echo "Please input model name"
         exit
 fi
 
-if [ ! -n "$PORT" ];then
+if ( ! -n "$PORT" );then
         echo "please input server port"
         exit
 fi
+LOG="experiments/logs/`date +'%m-%d'`/fl_server.log"
+echo Loggin output to "$LOG"
 
-nohup python3 fl_server.py --config_file data/task_configs/${DATASET}/${MODEL}_task.json --port ${PORT} > fl_server.log &
+nohup python3 fl_server.py --config_file data/task_configs/${DATASET}/${MODEL}_task.json --port ${PORT} > ${LOG} &
