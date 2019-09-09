9bf7b6aa8ec71d06704ef752cc233c1c0332e0d8
jiahuanluo <luojiahuan001@gmail.com>
2019-09-09 16:01:04 +0800

    fix bug

--- model/yolo.py
+++ model/yolo.py
@@ -236,6 +236,8 @@ class Darknet(nn.Module):
 
     def __init__(self, config_path, img_size=416):
         super(Darknet, self).__init__()
+        #config_path = "config/yolov3.cfg"
+        print(config_path)
         self.module_defs = parse_model_config(config_path)
         self.hyperparams, self.module_list = create_modules(self.module_defs)
         self.yolo_layers = [layer[0] for layer in self.module_list if hasattr(layer[0], "metrics")]
